7f73b97e7c853266d3928563a50a63578faf1c0f
陈晓聪 <a1194457876@outlook.com>
2019-12-18 20:04:49 +0800

    新增IO文件

---
+++ AnalogSignalAnalysisWpf/HamburgerMenuRipple/NewIOMeasurementViewModel.cs
@@ -0,0 +1,387 @@
+﻿using AnalogSignalAnalysisWpf.Hardware;
+using AnalogSignalAnalysisWpf.Hardware.Scope;
+using AnalogSignalAnalysisWpf.LiveData;
+using Caliburn.Micro;
+using DataAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using Framework.Infrastructure.Serialization;
+
+namespace AnalogSignalAnalysisWpf
+{
+    /// <summary>
+    /// 外部记录的数据
+    /// </summary>
+    public class ExternRecordData
+    {
+        //数据列表
+        public Dictionary<int, double> RecordVoltages = new Dictionary<int, double>();
+
+        /// <summary>
+        /// 最大电压
+        /// </summary>
+        public double MaxVoltage { get; set; }
+
+        /// <summary>
+        /// 最大时间
+        /// </summary>
+        public double MaxTime { get; set; }
+
+        /// <summary>
+        /// 时间间隔(MS)
+        /// </summary>
+        public int TimeInterval { get; set; } = 50;
+    }
+
+    public class NewIOMeasurementViewModel : Screen
+    {
+
+        /// <summary>
+        /// 内部锁
+        /// </summary>
+        private object lockObject = new object();
+
+        #region 构造函数
+
+        private bool isAdmin = false;
+
+        /// <summary>
+        /// 管理员权限
+        /// </summary>
+        public bool IsAdmin
+        {
+            get
+            {
+                return isAdmin;
+            }
+            set
+            {
+                isAdmin = value;
+                NotifyOfPropertyChange(() => IsAdmin);
+            }
+        }
+
+        /// <summary>
+        /// 系统参数管理器
+        /// </summary>
+        public SystemParamManager SystemParamManager { get; private set; }
+
+        /// <summary>
+        /// 创建InputOutputMeasurementViewModel新实例
+        /// </summary>
+        public NewIOMeasurementViewModel()
+        {
+            //恢复配置参数
+            SystemParamManager = SystemParamManager.GetInstance();
+
+            //更新硬件状态
+            UpdateHardware();
+        }
+
+        /// <summary>
+        /// 创建InputOutputMeasurementViewModel新实例
+        /// </summary>
+        /// <param name="scope">示波器</param>
+        /// <param name="power">Power</param>
+        public NewIOMeasurementViewModel(IScopeBase scope, IPower power, IPLC plc, IPWM pwm) : this()
+        {
+            if (scope == null)
+            {
+                throw new ArgumentException("scope invalid");
+            }
+
+            if (power == null)
+            {
+                throw new ArgumentException("power invalid");
+            }
+
+            if (plc == null)
+            {
+                throw new ArgumentException("plc invalid");
+            }
+
+            if (pwm == null)
+            {
+                throw new ArgumentException("pwm invalid");
+            }
+
+            Scope = scope;
+            Power = power;
+            PLC = plc;
+            PWM = pwm;
+
+            if (!IsHardwareValid)
+            {
+                RunningStatus = "硬件无效";
+            }
+            else
+            {
+                RunningStatus = "就绪";
+            }
+        }
+
+        #endregion
+
+        #region 运行状态
+
+        private bool isMeasuring;
+
+        /// <summary>
+        /// 测量标志
+        /// </summary>
+        public bool IsMeasuring
+        {
+            get
+            {
+                return isMeasuring;
+            }
+            set
+            {
+                isMeasuring = value;
+                NotifyOfPropertyChange(() => IsMeasuring);
+            }
+        }
+
+        private bool canMeasure = true;
+
+        /// <summary>
+        /// 允许测量
+        /// </summary>
+        public bool CanMeasure
+        {
+            get
+            {
+                if (IsHardwareValid)
+                {
+                    return canMeasure;
+                }
+
+                return false;
+            }
+            set
+            {
+                canMeasure = value;
+                NotifyOfPropertyChange(() => CanMeasure);
+            }
+        }
+
+        private string runningStatus = "就绪";
+
+        /// <summary>
+        /// 运行状态
+        /// </summary>
+        public string RunningStatus
+        {
+            get
+            {
+                return runningStatus;
+            }
+            set
+            {
+                runningStatus = value;
+                NotifyOfPropertyChange(() => RunningStatus);
+            }
+        }
+
+        private double currentInput;
+
+        /// <summary>
+        /// 当前输入电压(电源输出电压)
+        /// </summary>
+        public double CurrentInput
+        {
+            get
+            {
+                return currentInput;
+            }
+            set
+            {
+                currentInput = value;
+                NotifyOfPropertyChange(() => CurrentInput);
+            }
+        }
+
+        private double currentOutput;
+
+        /// <summary>
+        /// 当前输出电压(测量电压)
+        /// </summary>
+        public double CurrentOutput
+        {
+            get
+            {
+                return currentOutput;
+            }
+            set
+            {
+                currentOutput = value;
+                NotifyOfPropertyChange(() => CurrentOutput);
+            }
+        }
+
+        #endregion
+
+        #region 硬件接口
+
+        /// <summary>
+        /// 示波器接口
+        /// </summary>
+        public IScopeBase Scope { get; set; }
+
+        /// <summary>
+        /// Power接口
+        /// </summary>
+        public IPower Power { get; set; }
+
+        /// <summary>
+        /// PLC接口
+        /// </summary>
+        public IPLC PLC { get; set; }
+
+        /// <summary>
+        /// PWM接口
+        /// </summary>
+        public IPWM PWM { get; set; }
+
+        /// <summary>
+        /// 硬件有效标志
+        /// </summary>
+        public bool IsHardwareValid
+        {
+            get
+            {
+                if ((Scope?.IsConnect == true) &&
+                    (Power?.IsConnect == true) &&
+                    (PLC?.IsConnect == true) &&
+                    (PWM?.IsConnect == true))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 更新硬件
+        /// </summary>
+        public void UpdateHardware()
+        {
+            if (Scope?.IsConnect != true)
+            {
+                Scope?.Connect();
+            }
+
+            if (Power?.IsConnect != true)
+            {
+                Power?.Connect();
+            }
+
+            if (PLC?.IsConnect != true)
+            {
+                PLC?.Connect();
+            }
+
+            if (PWM?.IsConnect != true)
+            {
+                PWM?.Connect();
+            }
+
+            NotifyOfPropertyChange(() => IsHardwareValid);
+            NotifyOfPropertyChange(() => CanMeasure);
+        }
+
+        #endregion
+
+        #region 事件
+
+        /// <summary>
+        /// 测量开始事件
+        /// </summary>
+        public event EventHandler<EventArgs> MeasurementStarted;
+
+        /// <summary>
+        /// 测量开始事件
+        /// </summary>
+        protected void OnMeasurementStarted()
+        {
+            CanMeasure = false;
+            MeasurementStarted?.Invoke(this, new EventArgs());
+        }
+
+        /// <summary>
+        /// 测量完成事件
+        /// </summary>
+        public event EventHandler<EventArgs> MeasurementCompleted;
+
+        /// <summary>
+        /// 测量完成事件
+        /// </summary>
+        /// <param name="e"></param>
+        protected void OnMeasurementCompleted(EventArgs e)
+        {
+            CanMeasure = true;
+
+            RunningStatus = "成功";
+
+            PLC.PWMSwitch = false;
+            PLC.FlowSwitch = false;
+
+            if (Power?.IsConnect == true)
+            {
+                Power.IsEnableOutput = false;
+            }
+
+            lock (lockObject)
+            {
+                IsMeasuring = false;
+            }
+            MeasurementCompleted?.Invoke(this, e);
+        }
+
+        /// <summary>
+        /// 消息触发事件
+        /// </summary>
+        public event EventHandler<MessageRaisedEventArgs> MessageRaised;
+
+        /// <summary>
+        /// 触发消息触发事件
+        /// </summary>
+        /// <param name="messageLevel"></param>
+        /// <param name="message"></param>
+        /// <param name="exception"></param>
+        protected void OnMessageRaised(MessageLevel messageLevel, string message, Exception exception = null)
+        {
+            MessageRaised?.Invoke(this, new MessageRaisedEventArgs(messageLevel, message, exception));
+        }
+
+        #endregion
+
+
+        #region 应用
+
+        /// <summary>
+        /// 外部记录的数据
+        /// </summary>
+        public ExternRecordData ExternRecordData { get; set; }
+
+        /// <summary>
+        /// 电压转气压
+        /// </summary>
+        /// <param name="voltage"></param>
+        /// <returns></returns>
+        private double VoltageToPressure(double voltage)
+        {
+
+            return (voltage - SystemParamManager.SystemParam.GlobalParam.PressureZeroVoltage) * SystemParamManager.SystemParam.GlobalParam.PressureK;
+        }
+
+        #endregion
+    }
+}
